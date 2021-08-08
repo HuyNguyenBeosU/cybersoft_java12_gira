4fb0d36f6a1f24e5d9dfb7a43c27f10756dbbf77
HuyNguyenBeosU <huy23042016@gmail.com>
2021-08-08 17:04:49 +0700

    xong 1/2 role module
    - chuan hoa output
    -custome messsage

---
+++ src/main/java/cybersoft/javaBE/java12/gira/role/repository/ProgramRepository.java
@@ -0,0 +1,9 @@
+package cybersoft.javaBE.java12.gira.role.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cybersoft.javaBE.java12.gira.role.entity.Program;
+
+public interface ProgramRepository extends JpaRepository<Program, Long> {
+
+}
